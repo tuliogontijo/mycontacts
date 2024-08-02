b22ff1d4d8dc99a42873fe2c43cc5e7c0187dc04
Tulio Cesar Gontijo <gontijo.tulio@gmail.com>
2024-08-02 16:56:12 -0300

    feat(fe): create home page and refactoring the Header

--- fe/src/pages/Home/index.js
+++ fe/src/pages/Home/index.js
@@ -1,3 +1,65 @@
+import {
+  Card, Container, Header, ListContainer, InputSearchContainer,
+} from './styles';
+
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
+
 export default function Home() {
-  return 'Home';
+  return (
+    <Container>
+      <InputSearchContainer>
+        <input type="text" placeholder="Pesquisar contato" />
+      </InputSearchContainer>
+      <Header>
+        <strong>3 contatos</strong>
+        <a href="/">Novo contato</a>
+      </Header>
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+            <img src={arrow} alt="Arrow" />
+          </button>
+        </header>
+        <Card>
+          <div className="info">
+            <div className="contactName">
+              <strong>Tulio Gontijo</strong>
+              <small>Instagram</small>
+            </div>
+            <span>tulio@email.com</span>
+            <span>(34) 99999-5555</span>
+          </div>
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+        <Card>
+          <div className="info">
+            <div className="contactName">
+              <strong>Tulio Gontijo</strong>
+              <small>Instagram</small>
+            </div>
+            <span>tulio@email.com</span>
+            <span>(34) 99999-5555</span>
+          </div>
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+      </ListContainer>
+    </Container>
+  );
 }
