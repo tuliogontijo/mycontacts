35fc1542baeb88c6b9c815cbf81db59b4953c8e9
Tulio Cesar Gontijo <gontijo.tulio@gmail.com>
2024-08-06 16:31:38 -0300

    feat(fe): create component Button

---
+++ fe/src/components/Button.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export default styled.button`
+  width: 100%;
+  height: 52px;
+  background: ${({ theme }) => theme.colors.primary.main};
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+  font-size: 16px;
+  font-weight: bold;
+  color: #fff;
+  border-radius: 4px;
+  border: none;
+  transition: background 0.2s ease-in;
+
+  &:hover {
+    background: ${({ theme }) => theme.colors.primary.light};
+  }
+
+  &:active {
+    background: ${({ theme }) => theme.colors.primary.dark};
+  }
+
+  &[disabled] {
+    background: #ccc;
+    cursor: default;
+  }
+`;
--- fe/src/pages/NewContact/index.js
+++ fe/src/pages/NewContact/index.js
@@ -2,6 +2,7 @@ import PageHeader from '../../components/PageHeader';
 
 import { Container } from './styles';
 
+import Button from '../../components/Button';
 import Input from '../../components/Input';
 import Select from '../../components/Select';
 
@@ -14,6 +15,8 @@ export default function NewContact() {
         <option value="123">Instagram</option>
         <option value="321">Facebook</option>
       </Select>
+      <Button type="button">Salvar alterações</Button>
+      <Button type="button" disabled>Salvar alterações</Button>
     </Container>
   );
 }
