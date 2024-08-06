b0ba5278d360667acc734fddaa4c07e8b23666db
Tulio Cesar Gontijo <gontijo.tulio@gmail.com>
2024-08-06 16:20:41 -0300

    feat(fe): create components Input e Select

--- fe/src/pages/NewContact/index.js
+++ fe/src/pages/NewContact/index.js
@@ -1,5 +1,19 @@
 import PageHeader from '../../components/PageHeader';
 
+import { Container } from './styles';
+
+import Input from '../../components/Input';
+import Select from '../../components/Select';
+
 export default function NewContact() {
-  return <PageHeader title="Novo contato" />;
+  return (
+    <Container>
+      <PageHeader title="Novo contato" />
+      <Input type="text" placeholder="Nome" />
+      <Select>
+        <option value="123">Instagram</option>
+        <option value="321">Facebook</option>
+      </Select>
+    </Container>
+  );
 }
