9f8bbba6b0b5ee8d3827ad1532397df5da8f0e59
Tulio Cesar Gontijo <gontijo.tulio@gmail.com>
2024-08-23 15:17:24 -0300

    feat(fe): get input data and create handle to form submit

--- fe/src/components/ContactForm/index.js
+++ fe/src/components/ContactForm/index.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 
+import { useState } from 'react';
 import { ButtonContainer, Form } from './styles';
 
 import Button from '../Button';
@@ -8,20 +9,54 @@ import Input from '../Input';
 import Select from '../Select';
 
 export default function ContactForm({ buttonLabel }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [category, setCategory] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log({
+      name,
+      email,
+      phone,
+      category,
+    });
+  }
+
   return (
-    <Form>
+    <Form onSubmit={(e) => handleSubmit(e)}>
       <FormGroup>
-        <Input type="text" placeholder="Nome" />
+        <Input
+          placeholder="Nome"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
       </FormGroup>
       <FormGroup error="O formato do e-mail é inválido">
-        <Input type="email" placeholder="E-mail" error />
+        <Input
+          type="email"
+          placeholder="E-mail"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
       </FormGroup>
       <FormGroup>
-        <Input type="tel" placeholder="Telefone" />
+        <Input
+          type="tel"
+          placeholder="Telefone"
+          value={phone}
+          onChange={(event) => setPhone(event.target.value)}
+        />
       </FormGroup>
       <FormGroup>
-        <Select>
+        <Select
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        >
+          <option value="">Categoria</option>
           <option value="instagram">Instagram</option>
+          <option value="discord">Discord</option>
         </Select>
       </FormGroup>
       <ButtonContainer>
