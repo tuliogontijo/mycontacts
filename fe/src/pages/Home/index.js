7d41b6a7c63cd754c43e6afcfec71c5c3d9d9149
Tulio Cesar Gontijo <gontijo.tulio@gmail.com>
2024-09-16 11:40:04 -0300

    feat(fe): render contacts list with data from api

--- fe/src/pages/Home/index.js
+++ fe/src/pages/Home/index.js
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 
+import { useEffect, useState } from 'react';
 import {
   Card,
   Container,
@@ -13,42 +14,73 @@ import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
 
 export default function Home() {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/contacts')
+      .then(async (response) => {
+        const json = await response.json();
+        setContacts(json);
+      })
+      .catch((error) => {
+        console.log('erro', error);
+      });
+  }, []);
+
   return (
     <Container>
       {/* <Loader /> */}
       {/* <Modal danger /> */}
       <InputSearchContainer>
-        <input type="text" placeholder="Pesquisar contato" />
+        <input
+          type="text"
+          placeholder="Pesquisar contato"
+        />
       </InputSearchContainer>
       <Header>
-        <strong>3 contatos</strong>
+        <strong>
+          {contacts.length}
+          {contacts.length === 1 ? ' contato' : ' contatos'}
+        </strong>
         <Link to="/new">Novo contato</Link>
       </Header>
       <ListContainer>
         <header>
           <button type="button">
             <span>Nome</span>
-            <img src={arrow} alt="Arrow" />
+            <img
+              src={arrow}
+              alt="Arrow"
+            />
           </button>
         </header>
-        <Card>
-          <div className="info">
-            <div className="contactName">
-              <strong>Tulio Gontijo</strong>
-              <small>Instagram</small>
-            </div>
-            <span>tulio@email.com</span>
-            <span>(34) 99999-5555</span>
-          </div>
-          <div className="actions">
-            <Link to="/edit/123">
-              <img src={edit} alt="Edit" />
-            </Link>
-            <button type="button">
-              <img src={trash} alt="Delete" />
-            </button>
-          </div>
-        </Card>
+        {contacts
+          && contacts.map((contact) => (
+            <Card key={contact.id}>
+              <div className="info">
+                <div className="contactName">
+                  <strong>{contact.name}</strong>
+                  {contact.category_name && <small>{contact.category_name}</small>}
+                </div>
+                <span>{contact.email}</span>
+                <span>{contact.phone}</span>
+              </div>
+              <div className="actions">
+                <Link to={`/edit/${contact.id}`}>
+                  <img
+                    src={edit}
+                    alt="Edit"
+                  />
+                </Link>
+                <button type="button">
+                  <img
+                    src={trash}
+                    alt="Delete"
+                  />
+                </button>
+              </div>
+            </Card>
+          ))}
       </ListContainer>
     </Container>
   );
